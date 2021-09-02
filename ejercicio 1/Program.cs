5de891199f2c84a4c3e279c2d495283f58bec9c5
Cristofer Albornoz <cristofer.albornoz@virginiogomez.cl>
2021-09-02 12:31:51 -0400

    hola mundo

--- ejercicio 1/Program.cs
+++ ejercicio 1/Program.cs
@@ -7,6 +7,8 @@ namespace ejercicio_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hola mundo!");
+
         }
     }
 }
